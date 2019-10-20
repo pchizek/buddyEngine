4d33893058a7649fec3f1e2d9ebd29ed1b5d9992
Pat Chizek <patrick.chizek@comcast.net>
2019-10-19 23:57:53 -0400

    Added standard program clock

--- main.cpp
+++ main.cpp
@@ -14,17 +14,26 @@
  * 
  */
 
+#include <thread>
+#include <chrono>
+
+#define FPS 60
+#define INTERVAL 17
+
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
 #include "managers/objectManager2d.h"
 #include "loader/loader.h"
+#include "controls.h"
 
 using namespace sf;
 using namespace std;
 
 // TODO: Make this not global, as issues arise in Debug configuration and it is bad practice
 RenderWindow window(VideoMode(800, 600), "SFML works!");
+
+//int playerState[4];
 
 /* Get struct for all objects to be rendered in the level */
 int main()
@@ -42,20 +51,38 @@ int main()
 	/* Main event loop */
 	while (window.isOpen())
 	{
+
+		using clock = chrono::steady_clock;
+
+		clock::time_point nextTick = clock::now() + 17ms;
+
+		/* Process events */
 		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed) {
 				window.close();
 			}
+
 		}
 
+		/* Controls */
+		controlInput();
+		resolveState();
+
+		cameraCoords[0] = playerState[0];
+		cameraCoords[1] = playerState[1];
+
+		/* Render objects */
+
 		window.clear();
-		
 		int rc = renderBlocks();
 	
-		//window.draw(levelObjects.firstBlock->sprite);
 		window.display();
+
+		// Wait until next time
+		this_thread::sleep_until(nextTick);
+
 	}
 
 	return 0;
