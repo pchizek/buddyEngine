18b16e4c4fddcaf1459e4b7903305f80c872cfae
Pat Chizek <patrick.chizek@comcast.net>
2020-02-20 19:18:21 -0500

    Complete reorganization of how objects are loaded
    Object info extracted directly from tinyXML objects, stages organized into individual functions

--- loader/loader.cpp
+++ loader/loader.cpp
@@ -11,78 +11,20 @@
 #include <string>
 #include "tinyxml2.h"
 #include "loader.h"
-#include "..//managers/objectManager2d.h"
+#include <unordered_map>
+#include "..//managers/objManager2d.h"
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
+#define ASSET_PATH "resources\\textures\\"
+
 using namespace std;
 using namespace tinyxml2;
+using namespace engine;
 
-/* Extracts a set of sibling elements from an xml file 
- * 
- * Arguments:
- * firstRef: a pointer to the first node of a linked list that stores the extracted strings
- * parentElement: a pointer to the parent element of the elements that you want to extract from
- * elementName: a char array that is the name of the element you want to extract
- *
- * Returns: number of elements extracted from the xml file
- */
-int extractElementSet(node* firstRef, XMLElement* parentElement, const char elementName[]) {
-
-	// Find the first element of the type you want to extract
-	XMLElement* extract = parentElement->FirstChildElement(elementName);
-
-	unsigned int numElements = 1;
-
-	node* newNode = firstRef;
-
-	while (extract) {
-
-		/* Extract value */
-		string temp = (string)extract->GetText();
-
-		/* Put the data in the node  */
-		newNode->data = temp;
-
-		/* Define the new node as the end by making its next a NULL pointer */
-		newNode->next = NULL;
-
-		/* 4. If the Linked List is empty, then make the new node as head */
-		node* last = firstRef;
-
-		while (last->next != NULL) {
-			last = last->next;
-		}
-
-		/* Change the next pointer of what was the last node to point to the new last node */
-		if (newNode != firstRef) {
-			last->next = newNode;
-		}
-
-		/* Get the next sibling element of the XML */
-		parentElement = parentElement->NextSiblingElement();
-
-		/* If next sibling element is NULL, we're done extracting this set of elements */
-		if (!parentElement) {
-			return numElements;
-
-		}
-
-		/* If there are more elements, get the element to extract from */
-		extract = parentElement->FirstChildElement(elementName);
-
-		/* Allocate a new node for this string */
-		newNode = new node;
-
-		numElements++;
-
-	}
-
-	/* If there's nothing to extract, return 0 */
-	return 0;
-}
-
+/* Parser for extracting values from  xml documents */
+#pragma region parsing
 /* Parses a string to extract various integer and array values
  *
  * Arguments:
@@ -91,44 +33,210 @@ int extractElementSet(node* firstRef, XMLElement* parentElement, const char elem
  * (Overloaded)
  * tupleArray: an array of 2 integers (such as 2D coordinates)
  * num: a single integer
- * 
+ *
  *
  * Returns: 0 on success, -1 on error
  */
-int parse(string inputString, int tupleArray[2]) {
 
-	size_t length = inputString.length();
+void parse(string inputString, int *outputInt) {
 
-	if ((inputString[0] == '[') && (inputString[length - 1] == ']')) {
+	*outputInt = stoi(inputString);
 
-		//Find index of comma
-		if (inputString.find(',', 1) == string::npos) {
-			return -1;
+}
+
+void parse(string inputString, int outputArr[], int outputArrSize) {
+
+	size_t len = inputString.length();
+
+	if ((inputString[0] == '[') && (inputString[len - 1] == ']')) {
+
+		len = inputString.length();
+
+		size_t thisComma = 0, nextComma;
+
+		for (int i = 0; i < outputArrSize; i++) {
+
+			if (thisComma == string::npos) {
+				exception("Bad Array: Invalid length");
+			}
+
+			nextComma = inputString.find(',', thisComma+1);
+			
+			if (nextComma != string::npos) {
+				outputArr[i] = stoi(inputString.substr(thisComma+1, (nextComma - thisComma)));
+			}
+			else {
+				outputArr[i] = stoi(inputString.substr(thisComma+1, (len - 2 - thisComma)));
+			}
+
+			thisComma = nextComma;
+
 		}
 
-		size_t idx = 0;
+	}
+	else {
+		exception("Bad Array: Syntax");
+	}
 
-		for (int i = 0; i < 2; i++) {
-			inputString = inputString.substr(idx + 1);
-			tupleArray[i] = stoi(inputString, &idx, 10);
+}
+
+#pragma endregion
+
+/* Loader for individual assets, objects */
+#pragma region load_element
+
+void loadAsset(XMLElement* assetElement) {
+
+	XMLElement* assetChildElement;
+	
+	assetChildElement = assetElement->FirstChildElement("file");
+	const char* fileName = assetChildElement->GetText();
+
+	// Get filename
+	const char* aliasName = assetElement->Attribute("alias");
+	string assetKey;
+	if (aliasName) {
+		assetKey = string(aliasName);
+	}
+	else {
+		assetKey = string(fileName);
+		assetKey = assetKey.substr(0,assetKey.find("."));
+	}
+	string texturePath = string(ASSET_PATH).append(fileName);
+	
+	// Call function to import the texture
+	engine::importTexture(texturePath, assetKey);
+
+}
+
+void loadObject(XMLElement* objectElement) {
+
+	// Access the unordered map of textures
+	
+
+	XMLElement* objectChildElement;
+
+	/* World location under <grid> or <coords> */
+	int worldLoc[2];
+
+	if (objectElement->FirstChildElement("coords")) {
+		
+		objectChildElement = objectElement->FirstChildElement("coords");
+
+		// Extract from document 
+		parse(objectChildElement->GetText(),worldLoc,2);
+
+	}
+	else if (objectElement->FirstChildElement("grid")){
+		
+		objectChildElement = objectElement->FirstChildElement("grid");
+
+		// Extract from document
+		parse(objectChildElement->GetText(),worldLoc,2);
+
+		worldLoc[0] *= BLOCK_SIZE;
+		worldLoc[1] *= BLOCK_SIZE;
+
+	}
+	else {
+		exception("Object: Invalid location");
+	}
+
+	/* Layer */
+	objectChildElement = objectElement->FirstChildElement("layer");
+	int objLayer{};
+
+	if (objectChildElement) {
+
+		parse(objectChildElement->GetText(), &objLayer);
+		if ((objLayer >= MAX_LAYERS) || (objLayer < 0)) exception("Object: Invalid layer value");
+
+	}
+	else {
+		exception("Object: Missing layer");
+	}
+
+	/* Texture */
+	objectChildElement = objectElement->FirstChildElement("texture");
+	sf::Texture* objectTexture{};
+
+	if (objectChildElement) {
+
+		// Get the asset from the map
+		unordered_map<string, sf::Texture>::iterator asset = engine::textureMap.find(objectChildElement->GetText());
+	
+		// Check if asset is valid
+		if (asset == engine::textureMap.end()) {
+			exception("Object: Texture not found");
 		}
+		else {
+			objectTexture = &asset->second;
+		}
+	
+	}
+	else {
+		exception("Object: Invalid texture");
+	}
+
+
+	/* Block selection or rectangle */
+	int spriteRect[4];
+
+	if (objectElement->FirstChildElement("rect")) {
+
+		objectChildElement = objectElement->FirstChildElement("rect");
+
+		// Extract from document
+		parse(objectChildElement->GetText(), spriteRect, 4);
+
+	}
+
+	else if (objectElement->FirstChildElement("blockSelect")) {
+		int blockSelect;
+
+		objectChildElement = objectElement->FirstChildElement("blockSelect");
+
+		// Extract from document
+		parse(objectChildElement->GetText(), &blockSelect);
+
+		// Set texture rectangle
+		spriteRect[0] = 0;
+		spriteRect[1] = (32 * blockSelect);
+		spriteRect[2] = 32;
+		spriteRect[3] = 32;
 
 	}
 
 	else {
-		return -1;
+
+		// Set texture rectangle
+		spriteRect[0] = 0;
+		spriteRect[1] = 0;
+		spriteRect[2] = 0;
+		spriteRect[3] = 0;
 	}
 
-	return 0;
+	/* Construct object */
+	engine::object* newObject = new engine::object(worldLoc, objLayer, objectTexture, spriteRect);
+
+	/* Emplace object */
+	newObject->prevObject = engine::lastObject;
+	if (engine::firstObject == NULL) {
+		engine::firstObject = newObject;
+	}
+	else {
+		newObject->prevObject->nextObject = newObject;
+	}
+	newObject->prevObject = engine::lastObject;
+
+	engine::lastObject = newObject;
+
 }
 
-int parse(string inputString) {
+#pragma endregion
 
-	size_t length = inputString.length();
-
-	return stoi(inputString, &length, 10);
-
-}
+/* Loader for assets section, environment */
+#pragma region load_section
 
 /* Loads an entire level into memory from an xml file
  *
@@ -137,131 +245,62 @@ int parse(string inputString) {
  *
  * Returns: 0 on success, error codes
  */
-int loadLevel(const char filename[]) {
 
-	/* Declare the struct that holds all the objects */
-	extern struct objects levelObjects;
 
-	// Get the window that will display the level
-	extern sf::RenderWindow window;
+void loadAssets(XMLDocument* levelDoc) {
+
+	/* Get first asset in document */
+	XMLElement* asset =
+		levelDoc->FirstChildElement("level")->
+		FirstChildElement("assets")->
+		FirstChildElement("asset");
+
+	while (asset) {
+
+		loadAsset(asset);
+		asset = asset->NextSiblingElement();
+
+	}
+
+}
+
+void loadEnvironment(XMLDocument* levelDoc) {
+
+	/* Get first object in document */
+	XMLElement* object =
+		levelDoc->FirstChildElement("level")->
+		FirstChildElement("environment")->
+		FirstChildElement("object");
+
+	while (object) {
+
+		loadObject(object);
+		object = object->NextSiblingElement();
+
+	}
+}
+
+#pragma endregion
+
+/* Loader for entire level */
+#pragma region load_level
+void loadLevel(const char filename[]) {
 
 	XMLDocument levelDoc;
 
-		// Open file
-		XMLError loadError = levelDoc.LoadFile(filename);
+	// Open file
+	XMLError loadError = levelDoc.LoadFile(filename);
 
 	// Throw error
 	if (loadError) {
-		return loadError;
+		exception("Level: Cannot open document");
 	}
 
-	// Define elements
-	XMLElement* level = levelDoc.FirstChildElement("level");
+	// Load Assets
+	loadAssets(&levelDoc);
 
-	XMLElement* assets = level->FirstChildElement("assets");
-	XMLElement* blockset = assets->FirstChildElement("blockset");
+	// Load Objects
+	loadEnvironment(&levelDoc);
 
-	XMLElement* environment = level->FirstChildElement("environment");
-	XMLElement* blockElement = environment->FirstChildElement("block");
-
-
-	/* Level Characteristics */
-
-	// Assemble macro grid
-	//string grid = (string)level->FirstChildElement("macroSize")->GetText();
-
-	//int rc = parse(grid, macroGridDims);
-
-	/* Load blockset data from file */
-	node	setIDs;
-	node	setFileNames;
-	node	setNumBlocks;
-
-	int numBlocksets = extractElementSet(&setIDs,
-		blockset,
-		"setNum");
-	extractElementSet(&setFileNames,
-		blockset,
-		"file");
-	extractElementSet(&setNumBlocks,
-		blockset,
-		"numBlocks");
-
-	/* Create static vector to hold all textures */
-	static vector<sf::Texture*> texPtrs(numBlocksets);
-
-	/* Store all blockset textures */
-	for (int i = 0; i < numBlocksets; i++) {
-
-		if (i) {
-			texPtrs.push_back(new sf::Texture());
-		}
-		else {
-			texPtrs.front() = new sf::Texture();
-		}
-
-		string filepath = "resources/blocksets/" + setFileNames.data;
-		if (!texPtrs.back()->loadFromFile(filepath)) {
-			throw "Texture File could not be opened";
-		}
-	}
-
-	/* Load blocks data from file */
-	node	blockSetNode;
-	node	selectionNode;
-	node	gridCoordsNode;
-
-	int numBlocks = extractElementSet(&blockSetNode,
-		blockElement,
-		"set");
-	extractElementSet(&selectionNode,
-		blockElement,
-		"select");
-	extractElementSet(&gridCoordsNode,
-		blockElement,
-		"world");
-
-	/* Initialize all blocks */
-	block* newBlock = NULL;
-	levelObjects.numBlocks = numBlocks;
-
-	for (int i = 0; i < numBlocks; i++) {
-		
-		/* Get which blockset this block will use */
-		int blockSetNum = parse(blockSetNode.data);
-		
-		/* Get which block in the set it is */
-		int select = parse(selectionNode.data);
-
-		/* Get world grid coordinates of this block */
-		int gridCoords[2];
-		parse(gridCoordsNode.data,gridCoords);
-
-		/* Create the new block */
-		if (!i) {
-			newBlock = new block(texPtrs.at(blockSetNum), gridCoords, select, true);
-			levelObjects.firstBlock = newBlock;
-		}
-		else {
-			
-			newBlock->nextBlock = new block(texPtrs.at(blockSetNum), gridCoords, select, true);
-			newBlock = newBlock->nextBlock;
-		}
-
-		/* Go to the next object in all the linked lists */
-		if (blockSetNode.next){
-			blockSetNode = *blockSetNode.next;
-			selectionNode = *selectionNode.next;
-			gridCoordsNode = *gridCoordsNode.next;
-		}
-
-	}
-
-	// Load other images/props
-
-	// Load entities
-
-	// Load player
-
-	return 0;
 }
+#pragma endregion
